c87f55f0c1a76e400737aa3b3f459e74c13a585a
PinkR1ver <3180102330@zju.edu.cn>
2024-07-05 15:32:31 +0800

    Change read_plist

--- read_plist_web.py
+++ read_plist_web.py
@@ -29,6 +29,161 @@ def butter_lowpass_filter(data, cutoff, fs, order=5):
     filtered_data = signal.filtfilt(b, a, data)
     return filtered_data
 
+def average_downsample(data, window_size):
+    for i in range(0, len(data), window_size):
+        data[i:i+window_size] = np.mean(data[i:i+window_size])
+        
+    data = data[::window_size]
+    return data
+
+
+# def get_inflection_points(data):
+    
+#     inflection_points_index = np.zeros(len(data))
+#     data = np.array(data)
+#     for i in range(3, len(data)-3):
+#         flag = np.zeros(6)
+#         for index, j in enumerate([-3, -2, -1, 1 ,2, 3]):
+#             flag[index] = (data[i+j] > data[i])
+
+#         if flag.sum() == 6:
+#             inflection_points_index[i] = 1
+#         elif flag.sum() == 0:
+#             inflection_points_index[i] = -1
+    
+#     return inflection_points_index
+            
+
+# def inflection_points_connection(data, inflection_points_index):
+    
+#     inflection_high = 0
+#     inflection_low = 0
+#     inflection_list = []
+    
+#     for i in range(0, len(inflection_points_index)):
+#         if inflection_points_index[i] == 1 or inflection_points_index[i] == -1:
+#             inflection_list.append(i)
+#     inflection_list.append(len(data) - 1)
+
+#     data = np.array(data)
+#     data_connection = np.zeros(len(data))
+#     for i in range(0, len(data) - 1):
+#         if inflection_points_index[i] == 1:
+#             data_connection[i] = data[i]
+#             inflection_high = i
+#         elif inflection_points_index[i] == -1:
+#             data_connection[i] = data[i]
+#             inflection_low = i
+#         else:
+#             if i == 0:
+#                 data_connection[i] = data[i]
+#             elif i == len(data):
+#                 data_connection[i] = data[i]
+#             elif inflection_high > inflection_low:
+#                 inflection_tmp = inflection_list[inflection_list.index(inflection_high) + 1]
+#                 data_connection[i] = data[inflection_high] + (data[inflection_tmp] - data[inflection_high]) / (inflection_tmp - inflection_high) * (i - inflection_high)
+#             elif inflection_high < inflection_low:
+#                 inflection_tmp = inflection_list[inflection_list.index(inflection_low) + 1]
+#                 data_connection[i] = data[inflection_low] + (data[inflection_tmp] - data[inflection_low]) / (inflection_tmp - inflection_low) * (i - inflection_low)
+            
+            
+#     return data_connection    
+
+def get_key_points(data):
+    
+    key_points = [0]
+    for i in range(1, len(data)-1):
+        if data[i] > data[i-1] and data[i] > data[i+1]:
+            key_points.append(i)
+        elif data[i] < data[i-1] and data[i] < data[i+1]:
+            key_points.append(i)
+            
+    key_points.append(len(data)-1)
+    
+    return key_points
+    
+
+def data_connection(data, key_ponits_list):
+    
+    result = data.copy()
+
+    for i in range(1, len(key_ponits_list)):
+        result[key_ponits_list[i-1]:key_ponits_list[i] + 1] = np.linspace(data[key_ponits_list[i-1]], data[key_ponits_list[i]], key_ponits_list[i] - key_ponits_list[i-1] + 1)
+    
+    return result
+
+def hollow_downsampling(data, downsample_window=0):
+    
+    if downsample_window == 0:
+        
+        zero_interval = 0
+        zero_interval_min = 99
+        for i in range(len(data)):
+            if data[i] == 0:
+                zero_interval += 1
+            else:
+                zero_interval_min = min(zero_interval, zero_interval_min)
+                zero_interval = 0
+
+        downsample_window = zero_interval_min
+    
+    data_downsampled = []
+    for i in range(0, len(data), downsample_window):
+        if data[i:i+downsample_window].sum() == 0:
+            data_downsampled.append(0)
+        else:
+            data_downsampled.append(data[i:i+downsample_window].sum())
+            
+    return data_downsampled
+
+
+def quantification(data, threshold):
+    
+    data = np.array(data)
+    data = np.round(data / threshold) * threshold
+    
+    return data
+
+
+def get_diff(data):
+    
+    diff = np.diff(data)
+    diff = np.insert(diff, 0, 0)
+    
+    return diff
+
+def data_compress(data, compressed_ratio):
+    
+    key_points_cp = [(0, data[0])]
+    for i in range(1, len(data) - 1):
+        
+        if data[i] >= data[i-1] and data[i] >= data[i+1]:
+            key_points_cp.append((i, data[i]))
+        elif data[i] <= data[i-1] and data[i] <= data[i+1]:
+            key_points_cp.append((i, data[i]))
+    
+    key_points_cp.append((len(data)-1, data[-1]))
+    
+    for i in range(0, len(key_points_cp)):
+        key_points_cp[i] = (int(key_points_cp[i][0] * compressed_ratio), key_points_cp[i][1])
+    
+    data_len = key_points_cp[-1][0]
+    
+    data_cp = np.zeros(data_len)
+    for i in range(1, len(key_points_cp) - 1):
+        left = key_points_cp[i-1][0]
+        right = key_points_cp[i][0] + 1
+        
+        if right - left == 2:
+            data_cp[left] = key_points_cp[i-1][1]
+            continue
+        
+        left_start = key_points_cp[i-1][1]
+        right_stop = key_points_cp[i][1]
+        data_cp[left:right] = np.linspace(left_start, right_stop, right - left)
+    
+    return data_cp
+
 if __name__ == '__main__':
     
     base_path = os.path.dirname(__file__)
@@ -43,6 +198,8 @@ if __name__ == '__main__':
         low_pass_cutoff = st.slider('Low-pass filter cutoff frequency', 0.0, 1000.0, 50.0)
         low_pass_order = st.slider('Low-pass filter order', 1, 10, 5)
         moving_average_window = st.slider('Moving average window size', 1, 100, 5)
+        compress_ratio = st.slider('Compress ratio', 0.0, 1.0, 0.1)
+        
     
 
     st.title('Read Plist File')
@@ -100,12 +257,38 @@ if __name__ == '__main__':
     fig.line(range(len(data)), data, line_width=2)
     st.bokeh_chart(fig)
     
+    # st.markdown('### Get the Derivative of the Signal')
+    # st.write('We get the derivative of the signal to get the acceleration')
+    
+    # diff = get_diff(data)
+    
+    # fig = figure(title='Derivative of the Signal', x_axis_label='Data Index', y_axis_label='Amplitude', width=800, height=400)
+    # fig.line(range(len(diff)), diff, line_width=2)
+    # st.bokeh_chart(fig)
+    
     st.markdown('### Download Filtered Data')
-    st.write('Here\'s the final result')
+    st.write('Here\'s the final result, we did the key point detection and use straight line to connect them')
+    
+    # data = average_downsample(data, data_downsampling_window)
+    key_points_list = get_key_points(data)
+    data_connect = data_connection(data, key_points_list)
+    
+    # inflection_points_index = get_inflection_points(data)
+    # data = inflection_points_connection(data, inflection_points_index)
+    
+    # data = hollow_downsampling(data, downsample_window=data_downsampling_window)
+    # data = quantification(data, quantification_threshold)
+    
+    fig = figure(title='After doing connection', x_axis_label='Data Index', y_axis_label='Amplitude', width=800, height=400)
+    fig.line(range(len(data_connect)), data_connect, line_width=2)
+    st.bokeh_chart(fig)
+    
+    data = data_connect
+    data = data_compress(data, compress_ratio)
     
     fig = figure(title=f'{upload_file_name}', x_axis_label='Data Index', y_axis_label='Amplitude', width=800, height=400, y_range=(-40, 40))
-    _interval = len(data) // 20
-    _interval = _interval // 10 * 10
+    _interval = len(data) // 5
+    _interval = _interval // 5 * 5
     fig.xaxis.ticker = bokeh.models.tickers.SingleIntervalTicker(interval=_interval)
     fig.grid.grid_line_color = 'Black'
     fig.grid.grid_line_alpha = 0.2
--- test.py
+++ test.py
@@ -1,35 +1,29 @@
 import numpy as np
-from scipy.signal import firwin, lfilter
 import matplotlib.pyplot as plt
+from scipy import signal
 
-# 设定滤波器参数
-cutoff_freq = 0.2  # 截止频率，根据你的需要调整
+# 创建一个简单的信号，例如一个正弦波
 fs = 1000  # 采样频率，单位Hz
-taps = 100  # 滤波器的阶数
+t = np.linspace(0, 1, fs, endpoint=False)  # 时间向量
+signal_original = np.sin(2 * np.pi * 5 * t)  # 5 Hz的正弦波
 
-# 设计FIR高通滤波器系数
-b = firwin(taps, cutoff_freq=cutoff_freq, window=('hamming',), pass_zero=False)
-
-# 生成测试信号
-t = np.linspace(0, 1, fs, endpoint=False)
-signal = np.sin(2 * np.pi * 50 * t) + 0.5 * np.sin(2 * np.pi * 150 * t)
-
-# 应用高通滤波器
-filtered_signal = lfilter(b, 1, signal)
-
-# 绘制结果
+# 绘制原始信号
 plt.figure(figsize=(12, 6))
 plt.subplot(2, 1, 1)
-plt.plot(t, signal)
+plt.plot(t, signal_original)
 plt.title('Original Signal')
-plt.xlabel('Time [s]')
-plt.ylabel('Amplitude')
 
+# 压缩信号的时间尺度，例如压缩到原来的50%
+compressed_ratio = 0.1
+fs_new = int(fs * compressed_ratio)  # 新的采样频率
+signal_compressed = signal.resample(signal_original, fs_new)
+
+t_new = np.linspace(0, 1, fs_new, endpoint=False)  # 新的时间向量
+
+# 绘制压缩后的信号
 plt.subplot(2, 1, 2)
-plt.plot(t, filtered_signal)
-plt.title('High Pass Filtered Signal')
-plt.xlabel('Time [s]')
-plt.ylabel('Amplitude')
+plt.plot(t_new, signal_compressed)
+plt.title('Compressed Signal')
 
 plt.tight_layout()
 plt.show()
