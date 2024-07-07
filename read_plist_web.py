737c7436effb208510619d5173655ed970b3830f
PinkR1ver <3180102330@zju.edu.cn>
2024-07-08 03:42:31 +0800

    chore: Update .gitignore to include *.zip and *.svg files add test raw data here add different mode selection to main.py

--- read_plist_web.py
+++ read_plist_web.py
@@ -88,9 +88,8 @@ def average_downsample(data, window_size):
 #             elif inflection_high < inflection_low:
 #                 inflection_tmp = inflection_list[inflection_list.index(inflection_low) + 1]
 #                 data_connection[i] = data[inflection_low] + (data[inflection_tmp] - data[inflection_low]) / (inflection_tmp - inflection_low) * (i - inflection_low)
-            
-            
-#     return data_connection    
+
+#     return data_connection
 
 def get_key_points(data):
     
@@ -260,6 +259,8 @@ if __name__ == '__main__':
 
     with st.sidebar:
         
+        mode = st.selectbox('Select the mode', ['Example: Single File Processing Procedures Detail', 'Batch Processing'], index=0)
+        
         st.title('Control the filter parameters')
         
         sampling_frequency = st.slider('Sampling frequency', 0.0, 500.0, 50.0)
@@ -272,158 +273,161 @@ if __name__ == '__main__':
         quantification_threshold = st.slider('Quantification threshold', 0.0, 4.0, 0.5)
         
     
+    if mode == 'Example: Single File Processing Procedures Detail':
 
-    st.title('Read Plist File')
-    st.write('This is a simple example of reading a plist file and applying a high-pass filter, a low-pass filter and a moving average filter to the data.')
-    
-    upload_file = st.file_uploader('Upload a plist file', type=['plist'])
-    
-    if upload_file is None:
+        st.title('Read Plist File')
+        st.write('This is a simple example of reading a plist file and applying a high-pass filter, a low-pass filter and a moving average filter to the data.')
         
-        upload_file = os.path.join(base_path, '12344_left_VOG_lefteye_Horizontal.plist')
-        upload_file_name = '12344_left_VOG_lefteye_Horizontal.plist'
-        with open(upload_file, 'rb') as plist_file:
-            data = plistlib.load(plist_file)
-    
-    else:
+        upload_file = st.file_uploader('Upload a plist file', type=['plist'])
         
-        bytes_data = upload_file.getvalue()
-        data = plistlib.loads(bytes_data)
-        upload_file_name = upload_file.name
+        if upload_file is None:
+            
+            upload_file = os.path.join(base_path, '12344_left_VOG_lefteye_Horizontal.plist')
+            upload_file_name = '12344_left_VOG_lefteye_Horizontal.plist'
+            with open(upload_file, 'rb') as plist_file:
+                data = plistlib.load(plist_file)
+        
+        else:
+            
+            bytes_data = upload_file.getvalue()
+            data = plistlib.loads(bytes_data)
+            upload_file_name = upload_file.name
+        
+        st.markdown('### Original Signal')
+        
+        # plot original signal
+        fig = figure(title='Original Signal', x_axis_label='Time(s)', y_axis_label='Amplitude', width=800, height=400)
+        fig.line(range(len(data)), data, line_width=2)
+        st.bokeh_chart(fig)
+        
+        st.markdown('### High Pass Filter')
+        st.write('Firstly, the high-pass filter is applied to the original signal to correct the baseline drift.')
+        
+        data_filtered = butter_highpass_filter(data, high_pass_cutoff, sampling_frequency, high_pass_order)
+        data = data_filtered
+        
+        fig = figure(title='Signal After highpass filter', x_axis_label='Time(s)', y_axis_label='Amplitude', width=800, height=400)
+        time = np.linspace(0, len(data) / sampling_frequency, len(data))
+        fig.line(time, data, line_width=2)
+        st.bokeh_chart(fig)
+        
+        st.markdown('### Low Pass Filter')
+        st.write('Secondly, the low-pass filter is applied to the signal after high-pass filtering to remove high-frequency noise.')
+        
+        data_filtered = butter_lowpass_filter(data, low_pass_cutoff, sampling_frequency, low_pass_order)
+        data = data_filtered
+        
+        fig = figure(title='Signal After lowpass filter', x_axis_label='Time(s)', y_axis_label='Amplitude', width=800, height=400)
+        time = np.linspace(0, len(data) / sampling_frequency, len(data))
+        fig.line(time, data, line_width=2)
+        st.bokeh_chart(fig)
+        
+        st.markdown('### Moving Average Filter')
+        st.write('Finally, the moving average filter is applied to the signal after low-pass filtering to smooth the signal.')
+        
+        data_filtered = moving_average_filter(data, moving_average_window)
+        data = data_filtered
+        
+        fig = figure(title='Signal After moving average filter', x_axis_label='Time(s)', y_axis_label='Amplitude', width=800, height=400)
+        time = np.linspace(0, len(data) / sampling_frequency, len(data))
+        fig.line(time, data, line_width=2)
+        st.bokeh_chart(fig)
+        
+        # st.markdown('### Get the Derivative of the Signal')
+        # st.write('We get the derivative of the signal to get the acceleration')
+        
+        # diff = get_diff(data)
+        
+        # fig = figure(title='Derivative of the Signal', x_axis_label='Time(s)', y_axis_label='Amplitude', width=800, height=400)
+        # fig.line(range(len(diff)), diff, line_width=2)
+        # st.bokeh_chart(fig)
+        
+        st.markdown('### Key Point Detection and Connection')
+        st.write('Here\'s the final result, we did the key point detection and use straight line to connect them')
+        
+        # data = average_downsample(data, data_downsampling_window)
+        key_points_list = get_key_points(data)
+        data_connect = data_connection(data, key_points_list)
+        
+        # inflection_points_index = get_inflection_points(data)
+        # data = inflection_points_connection(data, inflection_points_index)
+        
+        # data = hollow_downsampling(data, downsample_window=data_downsampling_window)
+        # data = quantification(data, quantification_threshold)
+        
+        data = data_connect
+        
+        st.markdown('### Time Domain Data Compression and Quantification')
+        
+        fig = figure(title='After doing connection', x_axis_label='Time(s)', y_axis_label='Amplitude', width=800, height=400)
+        time = np.linspace(0, len(data) / sampling_frequency, len(data))
+        fig.line(time, data, line_width=2)
+        st.bokeh_chart(fig)
+        
+        data, key_points_list = data_compress(data, compress_ratio, key_points_list)
+        
+        st.write('After data compress in time domain, we get the following signal:')
+        
+        fig = figure(title='After Data Compress', x_axis_label='Time(s)', y_axis_label='Amplitude', width=800, height=400)
+        time = np.linspace(0, len(data) / sampling_frequency, len(data)) / compress_ratio
+        fig.line(time, data, line_width=2)
+        st.bokeh_chart(fig)
+        
+        data = quantification(data, quantification_threshold, key_points_list)
+        
+        st.write('After quantification, we get the following signal:')
+        
+        fig = figure(title='After Quantification', x_axis_label='Time(s)', y_axis_label='Amplitude', width=800, height=400)
+        time = np.linspace(0, len(data) / sampling_frequency, len(data)) / compress_ratio
+        fig.line(time, data, line_width=2)
+        st.bokeh_chart(fig)
+        
+        st.markdown('### Save the Result')
+        st.write('Here\'s the final result, you can download the graph and save the data to a .csv file.')
+        
+        fig = figure(title=f'{upload_file_name}', x_axis_label='Time(s)', y_axis_label='Amplitude', width=800, height=400, y_range=(-40, 40))
+        time = np.linspace(0, len(data) / sampling_frequency, len(data)) / compress_ratio
+        # _interval = len(data) // 2
+        # _interval = _interval // 2 * 2
+        # fig.xaxis.ticker = bokeh.models.tickers.SingleIntervalTicker(interval=_interval)
+        _interval = len(time) // 2 * 2
+        _interval = _interval // 2 * 2
+        fig.xaxis.ticker = bokeh.models.tickers.SingleIntervalTicker(interval=2)
+        fig.grid.grid_line_color = 'Black'
+        fig.grid.grid_line_alpha = 0.2
+        fig.line(time, data, line_width=2, line_color='red')
+        st.bokeh_chart(fig)
+        
+        acc = np.diff(data)
+        acc = np.insert(acc, 0, 0)
+        
+        result_csv = pd.DataFrame({'time': time, 'amplitude': data, 'acceleration': acc})
+        st.download_button('Download the result as a CSV file', result_csv.to_csv(), 'result.csv', 'text/csv')
     
-    st.markdown('### Original Signal')
+    if mode == 'Batch Processing':
     
-    # plot original signal
-    fig = figure(title='Original Signal', x_axis_label='Time(s)', y_axis_label='Amplitude', width=800, height=400)
-    fig.line(range(len(data)), data, line_width=2)
-    st.bokeh_chart(fig)
-    
-    st.markdown('### High Pass Filter')
-    st.write('Firstly, the high-pass filter is applied to the original signal to correct the baseline drift.')
-    
-    data_filtered = butter_highpass_filter(data, high_pass_cutoff, sampling_frequency, high_pass_order)
-    data = data_filtered
-    
-    fig = figure(title='Signal After highpass filter', x_axis_label='Time(s)', y_axis_label='Amplitude', width=800, height=400)
-    time = np.linspace(0, len(data) / sampling_frequency, len(data))
-    fig.line(time, data, line_width=2)
-    st.bokeh_chart(fig)
-    
-    st.markdown('### Low Pass Filter')
-    st.write('Secondly, the low-pass filter is applied to the signal after high-pass filtering to remove high-frequency noise.')
-    
-    data_filtered = butter_lowpass_filter(data, low_pass_cutoff, sampling_frequency, low_pass_order)
-    data = data_filtered
-    
-    fig = figure(title='Signal After lowpass filter', x_axis_label='Time(s)', y_axis_label='Amplitude', width=800, height=400)
-    time = np.linspace(0, len(data) / sampling_frequency, len(data))
-    fig.line(time, data, line_width=2)
-    st.bokeh_chart(fig)
-    
-    st.markdown('### Moving Average Filter')
-    st.write('Finally, the moving average filter is applied to the signal after low-pass filtering to smooth the signal.')
-    
-    data_filtered = moving_average_filter(data, moving_average_window)
-    data = data_filtered
-    
-    fig = figure(title='Signal After moving average filter', x_axis_label='Time(s)', y_axis_label='Amplitude', width=800, height=400)
-    time = np.linspace(0, len(data) / sampling_frequency, len(data))
-    fig.line(time, data, line_width=2)
-    st.bokeh_chart(fig)
-    
-    # st.markdown('### Get the Derivative of the Signal')
-    # st.write('We get the derivative of the signal to get the acceleration')
-    
-    # diff = get_diff(data)
-    
-    # fig = figure(title='Derivative of the Signal', x_axis_label='Time(s)', y_axis_label='Amplitude', width=800, height=400)
-    # fig.line(range(len(diff)), diff, line_width=2)
-    # st.bokeh_chart(fig)
-    
-    st.markdown('### Key Point Detection and Connection')
-    st.write('Here\'s the final result, we did the key point detection and use straight line to connect them')
-    
-    # data = average_downsample(data, data_downsampling_window)
-    key_points_list = get_key_points(data)
-    data_connect = data_connection(data, key_points_list)
-    
-    # inflection_points_index = get_inflection_points(data)
-    # data = inflection_points_connection(data, inflection_points_index)
-    
-    # data = hollow_downsampling(data, downsample_window=data_downsampling_window)
-    # data = quantification(data, quantification_threshold)
-    
-    data = data_connect
-    
-    st.markdown('### Time Domain Data Compression and Quantification')
-    
-    fig = figure(title='After doing connection', x_axis_label='Time(s)', y_axis_label='Amplitude', width=800, height=400)
-    time = np.linspace(0, len(data) / sampling_frequency, len(data))
-    fig.line(time, data, line_width=2)
-    st.bokeh_chart(fig)
-    
-    data, key_points_list = data_compress(data, compress_ratio, key_points_list)
-    
-    st.write('After data compress in time domain, we get the following signal:')
-    
-    fig = figure(title='After Data Compress', x_axis_label='Time(s)', y_axis_label='Amplitude', width=800, height=400)
-    time = np.linspace(0, len(data) / sampling_frequency, len(data)) / compress_ratio
-    fig.line(time, data, line_width=2)
-    st.bokeh_chart(fig)
-    
-    data = quantification(data, quantification_threshold, key_points_list)
-    
-    st.write('After quantification, we get the following signal:')
-    
-    fig = figure(title='After Quantification', x_axis_label='Time(s)', y_axis_label='Amplitude', width=800, height=400)
-    time = np.linspace(0, len(data) / sampling_frequency, len(data)) / compress_ratio
-    fig.line(time, data, line_width=2)
-    st.bokeh_chart(fig)
-    
-    st.markdown('### Save the Result')
-    st.write('Here\'s the final result, you can download the graph and save the data to a .csv file.')
-    
-    fig = figure(title=f'{upload_file_name}', x_axis_label='Time(s)', y_axis_label='Amplitude', width=800, height=400, y_range=(-40, 40))
-    time = np.linspace(0, len(data) / sampling_frequency, len(data)) / compress_ratio
-    # _interval = len(data) // 2
-    # _interval = _interval // 2 * 2
-    # fig.xaxis.ticker = bokeh.models.tickers.SingleIntervalTicker(interval=_interval)
-    _interval = len(time) // 2 * 2
-    _interval = _interval // 2 * 2
-    fig.xaxis.ticker = bokeh.models.tickers.SingleIntervalTicker(interval=2)
-    fig.grid.grid_line_color = 'Black'
-    fig.grid.grid_line_alpha = 0.2
-    fig.line(time, data, line_width=2, line_color='red')
-    st.bokeh_chart(fig)
-    
-    acc = np.diff(data)
-    acc = np.insert(acc, 0, 0)
-    
-    result_csv = pd.DataFrame({'time': time, 'amplitude': data, 'acceleration': acc})
-    st.download_button('Download the result as a CSV file', result_csv.to_csv(), 'result.csv', 'text/csv')
-    
-    st.markdown('### Batch Processing')
-    st.write('Above is example show, you can upload multiple files to process in batch.')
-    
-    upload_files = st.file_uploader('Upload multiple plist files', type=['plist'], accept_multiple_files=True)
-    
-    if upload_files:
+        st.markdown('### Batch Processing')
+        st.write('Above is example show, you can upload multiple files to process in batch.')
+        
+        upload_files = st.file_uploader('Upload multiple plist files', type=['plist'], accept_multiple_files=True)
+        
+        if upload_files:
 
-        result_csv_list, fig_list =  batch_processing(upload_files)
-        zipObj = ZipFile('result.zip', 'w')
-        for csv_name, csv_file in result_csv_list:
-            csv_file = csv_file.to_csv()
-            zipObj.writestr(csv_name, csv_file)
-        for fig_name, fig in fig_list:
-            fig_name = fig_name
-            fig.output_backend = 'svg'
-            bokeh.io.export_svgs(fig, filename=fig_name)
-            with open(fig_name, 'rb') as fig_file:
-                fig_data = fig_file.read()
-            zipObj.writestr(fig_name, fig_data)
-        zipObj.close()
-        st.download_button('Download the result as a ZIP file', open('result.zip', 'rb').read(), 'result.zip', 'application/zip')
-        
+            result_csv_list, fig_list =  batch_processing(upload_files)
+            zipObj = ZipFile('result.zip', 'w')
+            for csv_name, csv_file in result_csv_list:
+                csv_file = csv_file.to_csv()
+                zipObj.writestr(csv_name, csv_file)
+            for fig_name, fig in fig_list:
+                fig_name = fig_name
+                fig.output_backend = 'svg'
+                bokeh.io.export_svgs(fig, filename=fig_name)
+                with open(fig_name, 'rb') as fig_file:
+                    fig_data = fig_file.read()
+                zipObj.writestr(fig_name, fig_data)
+            zipObj.close()
+            st.download_button('Download the result as a ZIP file', open('result.zip', 'rb').read(), 'result.zip', 'application/zip')
+            
     
     
         
